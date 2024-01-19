bb09a58ca509e340f80fdd1a76cd1308b3a3f3b1
Maksim Tihomirov <maxbeere@yandex.ru>
2024-01-19 23:57:25 +0300

    some changes

--- packages/web/src/app/app.routes.ts
+++ packages/web/src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
     {
+        path: '',
+        redirectTo: '/demo',
+        pathMatch: 'full'
+    },
+    {
         path: 'demo',
         loadComponent: () => import('./components/demo-page/demo-page.component').then(component => component.DemoPageComponent)
     }
