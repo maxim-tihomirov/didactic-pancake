66451ebe48697a336ebd4a5b6a1ce8f48f8801e2
maxim-tihomirov <maxbeere@yandex.ru>
2024-01-25 22:10:53 +0300

    add taiga

--- packages/web/src/app/app.component.ts
+++ packages/web/src/app/app.component.ts
@@ -1,12 +1,20 @@
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiAlertModule,
+  TUI_SANITIZER,
+} from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
 })
 export class AppComponent {
   title = 'web';
